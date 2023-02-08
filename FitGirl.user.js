27d61129b33bb60b9f1ee7ab2540724cc710984c
Hossam Ismail <96880247+nomicwave@users.noreply.github.com>
2023-02-08 12:13:57 +0200

    Updated script name

--- FitGirl.user.js
+++ FitGirl.user.js
@@ -1,5 +1,5 @@
 // ==UserScript==
-// @name         FitGirl Dark-Mode
+// @name         FitGirl - Dark-Mode
 // @namespace    Nomicwave
 // @version      1.0
 // @description  Dark mode and visual enhancements.
